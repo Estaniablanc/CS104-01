e9da420c1ef5e0d31fe5482cb8da0425843ae9a1
Estaniablanc <71726093+Estaniablanc@users.noreply.github.com>
2020-10-06 09:46:35 -0400

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -2,15 +2,15 @@ temp = 0
 x = 0
 while x == 0:
     temp = int(input("please enter the current temperature "))
-    if temp == 900:
+    elif temp == 900:
         x = x+1
-    if temp > 90:
+    elif temp > 90:
         print("wear shorts")
-    if temp > 70:
+    elif temp > 70:
         print("short sleeves are fine")
-    if temp > 50:
+    elif temp > 50:
         print("wear a jacket")
-    if temp > 32:
+    elif temp > 32:
         print("wear a heavy coat")
     else:
         print("stay inside")
